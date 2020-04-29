43938dff79c1f811903d1f9d76786432364704dc
Lucas <araujolucas97@gmail.com>
2020-04-29 14:24:05 -0300

    Register Group and GroupandUser

--- src/controller/GroupContrller.js
+++ src/controller/GroupContrller.js
@@ -1,3 +1,5 @@
+const sequelize = require('../config/db');
+const UserGroupController = require('./UserGroupController');
 class GroupController {
     routes() {
         return {
@@ -8,10 +10,58 @@ class GroupController {
 
     registerGroup() {
         return (req, res) => {
+            let db = sequelize.models.Group;
+
+            if (req.body.name !== "" || req.body.name !== null || typeof req.body.name !== undefined) {
+
+                db.create({ name: req.body.name, description: req.body.description })
+                    .then(group => {
+
+                        let userGroupController = new UserGroupController;
+
+                        userGroupController.register(req.userId, group.id)
+                            .then(idSearch => {
+                                this.getGroups(idSearch, db)
+                                    .then(groups => {
+                                        res.status(200).json(groups);
+                                    })
+                                    .catch(error =>{
+                                        res.status(400).json(error);
+                                    })
+
+                            })
+                            .catch(error => {
+                                res.status(400).json({error, message: `error ao registar user e group`});
+                            });
+
+                    })
+                    .catch(error => {
+                        res.status(400).json({ error, success: false });
+                    })
+
+            } else {
+                res.status(400).json({ success: false, message: `Preencha o campo nome!` })
+            }
 
         }
     }
-
+    getGroups(idSearch, db) {
+        return new Promise((resolve, reject) => {
+            console.log(idSearch)
+            db.findAll({
+                where: {
+                    id: idSearch
+                }
+            })
+                .then(groups => {
+                    resolve(groups);
+                })
+                .catch(error => {
+                    let newError = {error, success: false, message:`erro ao retornar grupos`}
+                    reject(newError);
+                })
+        })
+    }
     getGroup() {
         return (req, res) => {
 
@@ -36,8 +86,10 @@ class GroupController {
         }
     }
 
-    permisioUser(){
-        return new Promise;
+    permisioUser() {
+        return new Promise((resolve, reject) => {
+
+        });
     }
 }
 
--- src/controller/UserGroupController.js
+++ src/controller/UserGroupController.js
@@ -1,5 +1,51 @@
-class UserGroupController{
-    
+const sequelize = require('../config/db');
+
+class UserGroupController {
+    register(idUser, idGroup) {
+        return new Promise((resolve, reject) => {
+            let db = sequelize.models.UserAndGroup;
+
+            db.create({ typeUser: 'ADM', fkUser: idUser, fkGroup: idGroup })
+                .then(() => {
+
+                    this.getGroupsForUser(idUser)
+                        .then(groups =>{
+                            resolve(groups);
+                        })
+                        .catch(error=>{
+                            reject(error);
+                        })
+
+                })
+                .catch(error => {
+                    let newError = { error, success: false, message: `error ao vincular grupo com usuário`};
+                    reject(newError);
+                })
+        })
+    }
+
+    getGroupsForUser(idUser) {
+        return new Promise((resolve, reject) => {
+            let db = sequelize.models.UserAndGroup;
+
+            db.findAll({where: {
+                fkUser: idUser
+            }})
+                .then(userGroups =>{
+
+                    let idsGroups = userGroups.map(element =>{
+                        return element.fkGroup;
+                    });
+                    
+                    resolve(idsGroups);
+                })
+                .catch(error =>{
+                    let newError = {error, success: false, message: `erro ao retornar grupos`};
+                    reject(newError);
+                })
+        })
+    }
+
 }
 
 module.exports = UserGroupController;
